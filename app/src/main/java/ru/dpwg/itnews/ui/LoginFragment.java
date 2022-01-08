51fbe9ef7e2ccc630c5791a363d28bba99318afc
Luda <ludo4ka21@yandex.ru>
2022-01-09 00:25:30 +0300

    refactoring

--- app/src/main/java/ru/dpwg/itnews/ui/LoginFragment.java
+++ app/src/main/java/ru/dpwg/itnews/ui/LoginFragment.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.inject.Inject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 import moxy.MvpAppCompatFragment;
 import moxy.presenter.InjectPresenter;
 import moxy.presenter.ProvidePresenter;
@@ -17,9 +21,16 @@ import ru.dpwg.itnews.R;
 import ru.dpwg.itnews.di.Di;
 import ru.dpwg.itnews.mvp.presenter.LoginPresenter;
 import ru.dpwg.itnews.mvp.view.LoginView;
+import timber.log.Timber;
 import toothpick.Toothpick;
 
 public class LoginFragment extends MvpAppCompatFragment implements LoginView {
+    Button buttonLogin;
+    Toolbar toolbar;
+    EditText emailEditText;
+    EditText passwordEditText;
+    View progressView;
+
     @InjectPresenter
     LoginPresenter loginPresenter;
 
@@ -36,5 +47,16 @@ public class LoginFragment extends MvpAppCompatFragment implements LoginView {
             @Nullable Bundle savedInstanceState
     ) {
         return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @androidx.annotation.Nullable @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        buttonLogin = view.findViewById(R.id.buttonLogin);
+        toolbar = view.findViewById(R.id.toolbar);
+        emailEditText = view.findViewById(R.id.emailEditText);
+        passwordEditText = view.findViewById(R.id.passwordEditText);
+        progressView = view.findViewById(R.id.progressView);
+        buttonLogin.setOnClickListener(v -> Timber.d("Кнопка нажата"));
     }
 }
