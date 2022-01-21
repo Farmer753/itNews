0a2b4484516add2002003f7bc986353fcb5e8443
Luda <ludo4ka21@yandex.ru>
2022-01-21 23:49:48 +0300

    Profile data models

---
+++ app/src/main/java/ru/dpwg/itnews/domain/user/NwUser.java
@@ -0,0 +1,11 @@
+package ru.dpwg.itnews.domain.user;
+
+import java.util.List;
+
+public class NwUser {
+
+    public String email;
+    public String fullName;
+    public String avatar;
+    public List<NwAuthority> authorities;
+}
