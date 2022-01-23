5060638565375dcc11af9354486903845c894dca
Luda <ludo4ka21@yandex.ru>
2022-01-23 15:16:39 +0300

    show user on screen

--- app/src/main/java/ru/dpwg/itnews/di/module/DomainModule.java
+++ app/src/main/java/ru/dpwg/itnews/di/module/DomainModule.java
@@ -1,11 +1,14 @@
 package ru.dpwg.itnews.di.module;
 
 import ru.dpwg.itnews.data.SessionRepositoryImpl;
+import ru.dpwg.itnews.data.UserRepositoryImpl;
 import ru.dpwg.itnews.domain.SessionRepository;
+import ru.dpwg.itnews.domain.user.UserRepository;
 import toothpick.config.Module;
 
 public class DomainModule extends Module {
     public DomainModule() {
         bind(SessionRepository.class).to(SessionRepositoryImpl.class).singleton();
+        bind(UserRepository.class).to(UserRepositoryImpl.class).singleton();
     }
 }
--- app/src/main/java/ru/dpwg/itnews/ui/ProfileFragment.java
+++ app/src/main/java/ru/dpwg/itnews/ui/ProfileFragment.java
@@ -5,10 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-
-import com.google.android.material.button.MaterialButton;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -18,15 +18,18 @@ import moxy.presenter.InjectPresenter;
 import moxy.presenter.ProvidePresenter;
 import ru.dpwg.itnews.R;
 import ru.dpwg.itnews.di.Di;
-import ru.dpwg.itnews.mvp.presenter.LoginPresenter;
+import ru.dpwg.itnews.domain.user.NwUser;
 import ru.dpwg.itnews.mvp.presenter.ProfilePresenter;
-import ru.dpwg.itnews.mvp.view.LoginView;
 import ru.dpwg.itnews.mvp.view.ProfileView;
 import toothpick.Toothpick;
 
 public class ProfileFragment extends MvpAppCompatFragment implements ProfileView {
 
     Button logout;
+    View progressView;
+    TextView emailTextView;
+    TextView nameTextView;
+
 
     @InjectPresenter
     ProfilePresenter profilePresenter;
@@ -54,7 +57,29 @@ public class ProfileFragment extends MvpAppCompatFragment implements ProfileView
         super.onViewCreated(view, savedInstanceState);
         logout = view.findViewById(R.id.buttonLogout);
         logout.setOnClickListener(v -> profilePresenter.onLogoutClick());
+        emailTextView = view.findViewById(R.id.emailTextView);
+        nameTextView = view.findViewById(R.id.nameTextView);
+        progressView = view.findViewById(R.id.progressView);
 
     }
 
+    @Override
+    public void showProgress(boolean show) {
+        if (show) {
+            progressView.setVisibility(View.VISIBLE);
+        } else {
+            progressView.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void showMessage(String message) {
+        Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void showUser(NwUser nwUser) {
+        emailTextView.setText(nwUser.email);
+        nameTextView.setText(nwUser.fullName);
+    }
 }
