4009550e1bd52f4d3ab52d691f7940c4c37f6d44
Luda <ludo4ka21@yandex.ru>
2022-02-05 23:40:58 +0300

    add NwArticle model

---
+++ app/src/main/java/ru/dpwg/itnews/domain/article/NwTranslation.java
@@ -0,0 +1,14 @@
+package ru.dpwg.itnews.domain.article;
+
+import java.util.List;
+
+public class NwTranslation {
+    public int id;
+    public int langId;
+    public int articleId;
+    public String title;
+    public String shortDescription;
+    public String imageUrl;
+    public String publishedDate;
+    public List<NwVersion> versions;
+}
---
+++ app/src/main/java/ru/dpwg/itnews/domain/article/NwVersion.java
@@ -0,0 +1,8 @@
+package ru.dpwg.itnews.domain.article;
+
+public class NwVersion {
+    public int id;
+    public int articleTranslationId;
+    public String text;
+    public String publishedDate;
+}
